6c62b7523f6764f3f50f8e2c96e02f86e9937873
Aryan Tomar <aryantomar239@gmail.com>
2025-02-17 12:29:10 +0530

    jim

---
+++ Day 1/p6.js
@@ -0,0 +1,15 @@
+const fs  = require('fs');
+
+const append = ()=>{
+    const data = "Hello, I am appending to a file ";
+    fs.appendFile("./data.txt",data,(err)=>{
+        if(err) 
+            console.log(err);
+         else console.log("Data appended successfully");
+        
+    });
+}
+
+console.log("I am before appending");
+append();
+console.log("I am after appending");
